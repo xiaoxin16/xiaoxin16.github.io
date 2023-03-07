a718631c31abae52a4f4f5e6cec1da5197d9b5e7
xiaxin16 <490482978@qq.com>
2023-03-07 19:01:25 +0800

    init OK

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230307.134153';
+const cacheName = 'chirpy-20230307.190036';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
