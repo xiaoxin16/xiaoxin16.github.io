6d495d00f7292fdb571807d159e7ab057f54bf97
xiaoxin16 <490482978@qq.com>
2023-03-15 21:20:18 +0800

    lalala

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230315.211613';
+const cacheName = 'chirpy-20230315.212009';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
