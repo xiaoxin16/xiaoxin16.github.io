c008b5b83bd187056de2f63fadb48edb33cd1c7a
xiaoxin16 <490482978@qq.com>
2024-02-18 19:45:41 +0800

    add busanzi

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240218.193129';
+const cacheName = 'chirpy-20240218.194529';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
